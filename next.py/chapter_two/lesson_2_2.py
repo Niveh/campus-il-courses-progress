127792f2e9a53d088a9d3e5a6c70b50664bf1cc4
Niveh <{ID}+{username}@users.noreply.github.com>
2022-07-28 01:25:20 +0300

    Complete lesson 2.2

---
+++ next.py/chapter_two/lesson_2_2.py
@@ -0,0 +1,30 @@
+
+class BankAccount:
+    def __init__(self) -> None:
+        self.balance = 0
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        self.balace -= amount
+
+    def print_balance(self):
+        print(f"Current balance is: {self.balance}")
+
+    def func(self):
+        print(self)
+
+
+def main():
+    customer1 = BankAccount()
+    customer1.deposit(400)
+    customer1.print_balance()
+    customer2 = BankAccount()
+    customer2.deposit(20)
+    customer2.print_balance()
+
+    customer1.func()
+
+
+main()
