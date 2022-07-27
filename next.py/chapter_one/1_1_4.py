c2b2311ac7201f225fc23e0fed0441e8f4d3d7fb
Niveh <{ID}+{username}@users.noreply.github.com>
2022-07-27 16:11:31 +0300

    Complete task 1.1.4

---
+++ next.py/chapter_one/1_1_4.py
@@ -0,0 +1,9 @@
+from functools import reduce
+
+
+def sum_of_digits(number):
+    return reduce((lambda total, n: total + int(n[0])), str(number), 0)
+
+
+print(sum_of_digits(104))
+print(sum_of_digits(111345))
