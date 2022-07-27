60825f4fd4dd9fb91f8c676ee436812bb3462115
Niveh <{ID}+{username}@users.noreply.github.com>
2022-07-28 01:31:02 +0300

    Complete task 2.2.2

---
+++ next.py/chapter_two/2_2_2.py
@@ -0,0 +1,24 @@
+
+class Octopus:
+    def __init__(self, name, age) -> None:
+        self.name = name
+        self.age = age
+
+    def birthday(self):
+        self.age += 1
+
+    def get_age(self):
+        return self.age
+
+
+def main():
+    octopus_one = Octopus("Octavio", 5)
+    octopus_two = Octopus("Octaviada", 5)
+
+    octopus_one.birthday()
+
+    print(octopus_one.get_age())
+    print(octopus_two.get_age())
+
+
+main()
