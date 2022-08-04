2641e6c2c10ecb20db818eb5b0b8eb7828066134
Niveh <{ID}+{username}@users.noreply.github.com>
2022-08-04 14:38:49 +0300

    Fix task to pass the test requirements

--- next.py/chapter_five/task_5_4.py
+++ next.py/chapter_five/task_5_4.py
@@ -1,4 +1,7 @@
 
+MAX_NUM = 999999999
+MAX_COUNT = 10
+
 
 def check_id_valid(id_number):
     """
@@ -32,47 +35,39 @@ def check_id_valid(id_number):
 class IDIterator:
     """
     ID Iterator class, used to generate valid ID numbers.
-    Takes an ID number (int) and a count (int) of valid IDs to generate.
-    ID defaults at 100000000 if not given.
-    Count defaults at -1 if not given,
-    iterating until reaching the limit (999999999).
+    Generates IDs upwards of the given number.
+    Iterating until generated 10 IDs or reached the limit (999999999).
     """
 
-    def __init__(self, user_id=100000000, count=-1) -> None:
+    def __init__(self, user_id=123456780) -> None:
         self._id = user_id
 
-        # We will be counting the amount of iterations to know when to stop.
+        # We will be counting the amount of IDs to know when to stop.
         self._ids_count = 0
-        self._ids_count_max = count
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        # Stop if we have reached the given valid IDs count requested,
-        # or maximum amount of iterations.
-        if self._ids_count_max != -1:
-            if self._ids_count >= self._ids_count_max or self._id >= 999999999:
-                raise StopIteration()
+        # Stop if we've generated 10 IDs or reached the last possible ID number.
+        if self._ids_count >= MAX_COUNT or self._id >= MAX_NUM:
+            raise StopIteration()
 
         # Try the next IDs until a valid ID is created.
-        self._id += 1
         while not check_id_valid(self._id):
             self._id += 1
 
         # Make sure we count the iterations if we aren't running to the limit.
-        if self._ids_count_max != -1:
-            self._ids_count += 1
+        self._ids_count += 1
 
-        # Handle edge case
-        if self._id >= 999999999:
-            raise StopIteration()
+        # Increment current ID value by one for the next iteration
+        self._id += 1
 
         # Return the most recent valid ID.
-        return self._id
+        return self._id - 1
 
 
-def id_generator(id_number=100000000):
+def id_generator(id_number=123456780):
     """
     Yields the next valid ID number between id_number and 999999999.
     :param id_number: ID number, starting point of ID numbers generation range
@@ -80,43 +75,38 @@ def id_generator(id_number=100000000):
     :return: Generator object holding the next valid ID number
     :rtype: Generator Object
     """
-    for i in range(id_number, 999999999):
+    for i in range(id_number, MAX_NUM):
         if check_id_valid(i):
             yield i
 
 
 def main():
     # Get user's input:
-    # ID number,
-    # method generation,
-    # amount of ID numbers to generate.
+    # ID number, method of generation.
     user_id = int(input("Enter ID: "))
     use_method = input("Generator or Iterator (gen/it)?: ").strip().lower()
-    user_count = int(
-        input("Enter amount of IDs to generate (-1 for all possible IDs): "))
+
+    # Make sure we have a valid generation method
+    while use_method not in "gen it":
+        print("Invalid generation method!")
+        use_method = input("Generator or Iterator (gen/it)?: ").strip().lower()
 
     try:
         if use_method == "gen":
             gen = id_generator(user_id)
-
-            # Make sure the input is either -1 or a positive number
-            if user_count == -1:
-                while True:
-                    print(next(gen))
-
-            elif user_count > 0:
-                for _ in range(user_count):
-                    print(next(gen))
-
-            else:
-                print("Invalid input for amount of IDs to generate!")
+            for _ in range(10):
+                print(next(gen))
 
         elif use_method == "it":
-            for i in IDIterator(user_id, user_count):
+            for i in IDIterator(user_id):
                 print(i)
 
     except StopIteration:
-        print("Finished generating ID numbers!")
+        print("\nStopIteration Exception: ID numbers generation reached it's limit.")
+
+    finally:
+        print("\nFinished generating ID numbers.")
+        print("Method: " + ("Iterator" if use_method == "it" else "Generator"))
 
 
 if __name__ == "__main__":
